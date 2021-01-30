701d39568631bea7512e40bedadceadc325aa268
Hai Jung Hsu <hhsu@adobe.com>
2021-01-30 11:26:30 -0700

    moved things into diff folder level for testing

---
+++ src/pages/Introduction/index.tsx
@@ -0,0 +1,47 @@
+import { IonContent, IonPage, IonButton, IonCol, IonRow, IonGrid } from '@ionic/react';
+import { language } from 'ionicons/icons';
+import React, { useState } from 'react';
+import { useParams } from 'react-router';
+import Splash from '../../components/core/Splash';
+import ExploreContainer from '../../components/ExploreContainer';
+import './styles.scss';
+
+const Introduction: React.FC = () => {
+
+  const [loadingSplash, setLoadingSplash] = useState<boolean>(true);
+
+  setTimeout(() => {
+    setLoadingSplash(false);
+  }, 1000);
+
+  // lan_select only accept input of 'eng' or 'spn'
+  const lan_select = (lan: "eng" | "spn") => {
+    console.log(lan);
+  }
+
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+        <Splash show={loadingSplash}></Splash>
+        <IonGrid id="lan_selector">
+          <IonRow>
+            <IonCol className="ion-text-center">
+              <IonButton color="light" size="large" expand="block" onClick={() => lan_select('eng')}>
+                English
+              </IonButton>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol className="ion-text-center">
+              <IonButton color="light" size="large" expand="block" onClick={() => lan_select('spn')}>
+                Españo
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Introduction;
